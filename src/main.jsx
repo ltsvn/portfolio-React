ff6d1c059db9f9140997273df06edabae29e2d5e
ltsvn <92997990+ltsvn@users.noreply.github.com>
2023-07-02 12:29:19 +0200

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,13 @@ import HomePage from "./components/pages/HomePage.jsx";
 import ParticlesBackground from "./components/ParticlesBackground.jsx";
 import SocialLinks from "./components/SocialLinks.jsx";
 import GeneralFooter from "./components/GeneralFooter.jsx";
+import AboutPage from "./components/pages/AboutPage.jsx";
 
-const router = createBrowserRouter([{path: "/portfolio-React", element: <HomePage/>}], {basename: 'portfolio-React'})
+const router = createBrowserRouter([
+    {path: "/portfolio-React", element: <HomePage/>},
+    {path: "/portfolio-React/about-me", element: <AboutPage/>}
+
+], {basename: '/portfolio-React'})
 
 
 //
